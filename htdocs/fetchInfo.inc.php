e2bca6cf29178a76fe4469ee62159d4bb9c805de
Neo <156038874+NEO110705@users.noreply.github.com>
2024-05-05 00:39:56 +0000

    Removed file

--- htdocs/fetchInfo.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-          $_SESSION["userId"] = 7; //change this id to whoever is currently logged in
-          $sessionId = $_SESSION["userId"];
-          $user = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM user WHERE userId = $sessionId"));
-?>
